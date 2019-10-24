88a97e4c93d46eceec7842abde8d7eb8c809987b
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-10-24 23:03:18 +0900

    入力した値を管理できるようにした

--- src/components/AkumaForm.js
+++ src/components/AkumaForm.js
@@ -1,16 +1,30 @@
 import React from 'react'
 
 class AkumaForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: ""
+        }
+    }
     render() {
         return(
             <div id="akuma-form">
                 <h1>数字の真実を…</h1>
                 <div>
-                    <input type="text" />
-                    <button type="button" >解き明かす…</button>
+                    <input type="text" value={this.state.input} onChange={this.handleChange}/>
+                    <button type="button" onClick={this.handleClick}>解き明かす…</button>
                 </div>
             </div> 
         );
+    }
+
+    handleChange = e => {
+        this.setState({input: e.currentTarget.value});
+    }
+
+    handleClick = e => {
+        console.log(this.state.input);
     }
 }
 export default AkumaForm;
