0eb99a166902af6cf129eb6ae4912687a5be806b
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-10-25 22:17:13 +0900

    dispatchを記述

---
+++ src/actions/inputValue.js
@@ -0,0 +1,10 @@
+const INPUT_VALUE='INPUT_VALUE';
+
+function inputValue(input) {
+    return {
+        type: INPUT_VALUE,
+        input: input
+    }
+}
+
+export default inputValue;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import {connect} from 'react-redux'
-
 import App from '../components/App'
-
+import inputValue from '../actions/inputValue'
 function mapStateToProps(state) {
     return state;
 }
 
 function mapDispatchToProps(dispatch) {
     return {
-
-    }
+        handleClick(input) {
+            dispatch(inputValue(input));
+        },
+    };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
