545b2712d25750c36a6553c78122a74204a0f34e
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-10-27 00:02:41 +0900

    react-bootstrapを使用する

--- src/components/AkumaNotResult.js
+++ src/components/AkumaNotResult.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Img from '../images/isNuN_mozaiku.jpg'
 
 class AkumaNotResult extends React.Component {
     render() {
         return(
-            <div>akumanot</div>
+            <div>
+                <h2>数字以外は入れちゃダメだってこと</h2>
+                <img src={Img}></img>
+            </div>
         );
     }
 }
