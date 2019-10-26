3d6af8391ae261790ac51d6ccfa5cf3e5f14e0d6
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-10-26 13:13:11 +0900

    変数にした

--- src/components/AkumaResult.js
+++ src/components/AkumaResult.js
@@ -2,16 +2,21 @@ import React from 'react'
 import AkumaView from './AkumaView'
 import AkumaChart from './AkumaChart';
 import AkumaNotResult from './AkumaNotResult';
+import { thisExpression } from '@babel/types';
 
 class AkumaResult extends React.Component {
 
+    construcor(props) {
+        super(props);
+        let {inputResult} = this.props;
+        this.inputResult = inputResult;
+    }
     render() {
-        const {inputResult} = this.props;
-        {if (inputResult === '') return <div></div>}
-        {if (isNaN(inputResult)) return <AkumaNotResult />}
+        {if (this.inputResult === '') return <div></div>}
+        {if (isNaN(this.inputResult)) return <AkumaNotResult />}
         return(
             <div>
-                {inputResult > 0? <AkumaView inputValue={this.props.inputResult}/> : <AkumaChart /> }
+                {this.inputResult > 0? <AkumaView inputValue={this.inputResult}/> : <AkumaChart /> }
             </div>
         );
     }
