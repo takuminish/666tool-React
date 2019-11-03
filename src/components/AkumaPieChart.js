6c0847a953b2666e6ecbb97a31d9ed6a80e6316e
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-11-03 21:33:16 +0900

    円グラフの見た目が完成

--- src/components/AkumaPieChart.js
+++ src/components/AkumaPieChart.js
@@ -1,22 +1,31 @@
 import React from 'react'
-import {Line} from 'react-chartjs-2'
+import {Pie} from 'react-chartjs-2'
 
 class AkumaPieChart extends React.Component {
     render() {
         const data = {
-          labels: ['April', 'May', 'June', 'July', 'August', 'September'],
+          labels: ['6', '666', 'other'],
           datasets: [
             {
-              data: [67, 79, 52, 41, 66, 43],
+              data: [10,10,10],
+              backgroundColor: [
+                  '#9400d3',  // darkViolet
+                  '#ffff00',  // yellow
+                  '#808080'   //gray
+              ,]
               // 省略
             },
           ],
         };
         const options = {
-          // 省略
+            legend: {
+                labels: {
+                    fontColor: "white"
+                }
+            }
         };
         return (
-          <Line data={data} options={options} />
+          <Pie data={data} options={options} />
         );
       
     }
